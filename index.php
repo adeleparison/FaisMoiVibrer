2c6e7d6e0b8ecac28c97fe0f44bec0c87fd3649a
adeleparison <adele.parison@hotmail.fr>
2018-04-04 13:54:53 +0200

    index.php = page accueil

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php require('includes/config.php');
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>FaisMoiVibrer</title>
+    <link rel="stylesheet" href="style/normalize.css">
+    <link rel="stylesheet" href="style/main.css">
+  </head>
+  <body>
+    <div id="wrapper">
+
+      <h1>Fais Moi Vibrer</h1>
+      <hr />
+      <a href="articles.php">articles</a>
+      <a href="produits.php">toys</a>
+    </div>
+
+  </body>
+</html>
